f6aaeb40853b8ff3ccb0c5f18dbdc731893a4e0c
MahmutYnc <mahmutla33@gmail.com>
2020-09-17 16:50:17 +0300

    Learnin Basics of Web-api

---
+++ LearningBasics/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningBasics.Controllers
+{
+    [Route("Products")]
+    public class ProductsController : ControllerBase
+    {
+        [Route("Merhaba")]
+        public String Merhaba()
+        {
+            return "Merhabalar ";
+        }
+
+        [Route("Hello")]
+        public String Hello()
+        {
+            return "Hello ";
+        }
+    }
+}
