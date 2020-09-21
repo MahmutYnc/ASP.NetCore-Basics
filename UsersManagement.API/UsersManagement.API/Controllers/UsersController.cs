f6ed1f67d163f57c3cbca0f3b41ab7ffdfdfdcd5
MahmutYnc <mahmutla33@gmail.com>
2020-09-21 17:03:34 +0300

    http methods with .net core

--- UsersManagement.API/UsersManagement.API/Controllers/UsersController.cs
+++ UsersManagement.API/UsersManagement.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,38 @@ namespace UsersManagement.API.Controllers
     {
         private List<User> _users = FakeData.GetUsers(50);
         //public String Get() => "Heyyyyyyyyy im back sons of bitches";
+        [HttpGet]
         public List<User> Get() => _users;
+        [HttpGet(("{id}"))]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+
+        [HttpPost]
+        public User Post([ FromBody ]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            User editUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            editUser.Firstname = user.Firstname;
+            editUser.Lastname = user.Lastname;
+            editUser.Address = user.Address;
+            return user;
+        }
+
+        [HttpDelete(("{id}"))]
+        public void Delete(int id)
+        {
+            var deletedUser =_users.FirstOrDefault(x => x.Id == id);
+            _users.Remove(deletedUser);
+        }
     }
 }
