82396023e62fe01fe2ac23aa0780b409ead41bb2
MahmutYnc <mahmutla33@gmail.com>
2020-09-23 02:21:08 +0300

    Hotel finder web application done with core api

--- HotelFinder/HotelFinder.DataAccess/Abstract/IHotelRepository.cs
+++ HotelFinder/HotelFinder.DataAccess/Abstract/IHotelRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using HotelFinder.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HotelFinder.DataAccess.Abstract
 {
-    interface IHotelRepository
+    public interface IHotelRepository
     {
+        List<Hotel> GetAllHotels();
+
+        Hotel GetHotelById(int id);
+
+        Hotel CreateHotel(Hotel hotel);
+
+        Hotel UpdateHotel(Hotel hotel);
+
+        void DeleteHotel(int id);
     }
 }
